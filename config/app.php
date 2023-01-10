c9d54eb2ce154fde2dd6d48c11c3eeeb6156bde0
Ali Shahidi <alishahidi1376@gmail.com>
2023-01-10 18:09:03 +0330

    composer update and add to packagist + bugfix

--- config/app.php
+++ config/app.php
@@ -4,7 +4,7 @@ return [
     'APP_TITLE' => 'apantos project',
     'TOKEN' => 'env',
     'CRYPT_TOKEN' => 'env',
-    'BASE_URL' => (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') ? 'https://'.$_SERVER['HTTP_HOST'] : 'http://'.$_SERVER['HTTP_HOST'],
+    'BASE_URL' => (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') ? 'https://' . $_SERVER['HTTP_HOST'] : 'http://' . $_SERVER['HTTP_HOST'],
     'BASE_DIR' => dirname(__DIR__),
     'ERRORS' => [
         '400' => 'errors.400',
@@ -14,7 +14,6 @@ return [
     // providers
     'PROVIDERS' => [
         \App\Providers\AppServiceProvider::class,
-        \App\Providers\SessionServiceProvider::class,
     ],
     'UN_VERIFY_TOKEN_ROUTE' => [],
 ];
