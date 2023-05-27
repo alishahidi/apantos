0ce4d7ebe9e1a84ee0cccfee9a6441af8be4e300
ahmadreza1383 <ahmadrezabashari@gmail.com>
2023-05-27 23:30:28 +0330

    Added class Controller for run in Route section

--- system/Router/Routing.php
+++ system/Router/Routing.php
@@ -2,10 +2,8 @@
 
 namespace System\Router;
 
-use ReflectionMethod;
 use stdClass;
 use System\Config\Config;
-use System\Router\Compare;
 
 class Routing extends stdClass
 {
@@ -48,56 +46,10 @@ class Routing extends stdClass
         return (new Find($reservedRoutes, $this->current_route))->handle();
     }
 
-    private function checkEmptyMatch()
-    {
-        if (empty($this->match)) $this->error404();
-    }
-
-    private function checkExistsClass()
-    {
-        $classPath = str_replace('\\', '/', $this->match['class']);
-        $path = Config::get('app.BASE_DIR').'/app/Http/Controllers/'.$classPath.'.php';
-
-        if (! file_exists($path)) $this->error404();
-    }
-
-    private function runController()
-    {
-        $class = "\App\Http\Controllers\\".$this->match['class'];
-        $object = new $class();
-
-        if (! method_exists($object, $this->match['method']))
-            $this->error404();
-
-        $reflection = new ReflectionMethod($class, $this->match['method']);
-        $parameterCount = $reflection->getNumberOfParameters();
-
-        if (! ($parameterCount <= count($this->match['parameters'])))
-            $this->error404();
-
-        call_user_func_array([$object, $this->match['method']], $this->match['parameters']);
-    }
-
     public function run()
     {
         $this->match = $this->matchMethod();
 
-        $this->checkEmptyMatch();
-
-        $this->checkExistsClass();
-
-        $this->runController();
-    }
-
-    public function error404()
-    {
-        http_response_code(404);
-        header($_SERVER['SERVER_PROTOCOL'].' 404 Not Found');
-        $view404 = Config::get('app.ERRORS.404');
-
-        if (! $view404) view('errors.404');
-        view($view404);
-
-        exit;
+        (new Controller($this->match))->handle();
     }
 }
