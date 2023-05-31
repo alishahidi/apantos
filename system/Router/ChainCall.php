2b82d9533e4837791ea894c128d4243ab4d7c3b9
ahmadreza1383 <ahmadrezabashari@gmail.com>
2023-05-31 22:49:04 +0330

    Optimized ChainCall in Route section

--- system/Router/ChainCall.php
+++ system/Router/ChainCall.php
@@ -2,19 +2,42 @@
 
 namespace System\Router;
 
+use Exception;
+
 class ChainCall
 {
     private static $class;
 
+    private static $params;
+
     public function __call($method, $args)
     {
-        if(empty(self::$class))
-            return (new static)->$method(...$args);
+        if(empty(self::class)) return (new $this);
 
-        $result = (new self::$class)->$method(...$args);
+        return $this->handle($method, $args);
+    }
 
-        if(empty($result))
-            return new self;
+    public static function __callStatic($method, $args)
+    {
+        if(! method_exists(self::class, $method))
+            return throw new Exception("Cannot find method {$method}");
+
+        (new self)->$method(...$args);
+
+        return new self;
+    }
+
+    private function instanse($class, $params = null)
+    {
+        self::$class = $class;
+
+        if(!empty($params));
+            self::$params = $params;
+    }
+
+    private function handle($method, $args)
+    {
+        $result = $this->call($method, $args);
 
         if(is_object($result))
             return (new $result);
@@ -22,18 +45,13 @@ class ChainCall
         return $result;
     }
 
-    public static function __callStatic($method, $args)
+    private function call($method, $args)
     {
-        if(! method_exists(self::class, $method))
-            return (new static)->$method(...$args);
+        $params = self::$params;
 
-        (new self)->$method(...$args);
+        if(empty($params) || is_null($params))
+            return ((new self::$class)->$method(...$args));
 
-        return new self;
-    }
-
-    private function call($class)
-    {
-        self::$class = $class;
+        return ((new self::$class(...$params))->$method(...$args));
     }
 }
